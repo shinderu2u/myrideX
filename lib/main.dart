c46c3ec86a5defdf33dd2de98145dcb426fc43a3
nazmi <amirulnazmi99@gmail.com>
2020-06-23 12:42:57 +0800

    myrideX

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:myridex/screen/home.dart';
+import 'package:myridex/states/app_state.dart';
 import 'package:myridex/utils/core.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+
+void main() {
+
+  Provider.debugCheckInvalidValueType = null;
+
+  return runApp(MultiProvider(providers: [
+        Provider(create:(_) =>AppState(),)
+  ],
+    child: MyApp(),));
+}
 
 class MyApp extends StatelessWidget {
 
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,158 +1,168 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:myridex/utils/core.dart';
+import 'package:myridex/states/app_state.dart';
+import 'package:provider/provider.dart';
 
 
-class MyHomePage extends StatefulWidget{
-  MyHomePage({Key key,this.title}) : super(key:key);
-  final String title;
+class MyHomePage extends StatefulWidget {
+MyHomePage({Key key, this.title}) : super(key: key);
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+final String title;
 
-
+@override
+_MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Map()
-    );
+    return Scaffold(body: Map());
   }
 }
 
 class Map extends StatefulWidget {
-
   @override
   _MapState createState() => _MapState();
-
 }
 
 class _MapState extends State<Map> {
-  GoogleMapController mapController;
-  static const _initialPosition = LatLng( 37.422, -122.084 );
-  LatLng _lastPostion = _initialPosition;
-  final Set<Marker> _markers ={};
-
-  get locationController => null;
-
-  get destinationController => null;
-
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-          body: Stack(
-              children: <Widget>[
-                GoogleMap( initialCameraPosition: CameraPosition(
-                    target: _initialPosition,
-                    zoom: 10 ),
-                  onMapCreated: onCreated,
-                  myLocationEnabled: true,
-                  mapType: MapType.normal,
-                  compassEnabled:  true,
-                  onCameraMove: _onCameraMove,
-                ),
-                Positioned(
-                  top: 50.0,
-                  right: 15.0,
-                  left: 15.0,
-                  child: Container(
-                    height: 50.0,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(3.0),
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey,
-                            offset: Offset(1.0, 5.0),
-                            blurRadius: 10,
-                            spreadRadius: 3)
-                      ],
-                    ),
-                    child: TextField(
-                      cursorColor: Colors.black,
-                      controller: locationController,
-                      decoration: InputDecoration(
-                        icon: Container(
-                          margin: EdgeInsets.only(left: 20, top: 5),
-                          width: 10,
-                          height: 10,
-                          child: Icon(
-                            Icons.location_on,
-                            color: green,
-                          ),
-                        ),
-                        hintText: "pick up",
-                        border: InputBorder.none,
-                        contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
-                      ),
-                    ),
-                  ),
-                ),
-
-                Positioned(
-                  top: 105.0,
-                  right: 15.0,
-                  left: 15.0,
-                  child: Container(
-                    height: 50.0,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(3.0),
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey,
-                            offset: Offset(1.0, 5.0),
-                            blurRadius: 10,
-                            spreadRadius: 3)
-                      ],
-                    ),
-                    child: TextField(
-                      cursorColor: Colors.black,
-                      controller: destinationController,
-                      textInputAction: TextInputAction.go,
-                      onSubmitted: (value) {
-                        sendRequest(value);
-                      },
-                      decoration: InputDecoration(
-                        icon: Container(
-                          margin: EdgeInsets.only(left: 20, top: 5),
-                          width: 10,
-                          height: 10,
-                          child: Icon(
-                            Icons.motorcycle,
-                            color: green,
-                          ),
-                        ),
-                        hintText: "destination?",
-                        border: InputBorder.none,
-                        contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
-                      ),
-                    ),
-                  ),
-                ),     
-              ]
+  @override
+  Widget build(BuildContext context) {
+    final appState = Provider.of<AppState>(context);
+    return SafeArea(
+      child: appState.initialPosition == null
+          ? Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SpinKitRotatingCircle(
+                    color: Colors.black,
+                    size: 50.0,
+                  )
+                ],
+              ),
+              SizedBox(height: 10,),
+              Visibility(
+                visible: appState.locationServiceActive == false,
+                child: Text("Please enable location services!", style: TextStyle(color: Colors.grey, fontSize: 18),),
+              )
+            ],
           )
-      );
-    }
+      )
+          : Stack(
+        children: <Widget>[
+          GoogleMap(
+            initialCameraPosition: CameraPosition(
+                target: appState.initialPosition, zoom: 10.0),
+            onMapCreated: appState.onCreated,
+            myLocationEnabled: true,
+            mapType: MapType.normal,
+            compassEnabled: true,
+            markers: appState.markers,
+            onCameraMove: appState.onCameraMove,
+            polylines: appState.polyLines,
+          ),
 
-  void onCreated(GoogleMapController controller){
-    setState(() {
-      mapController = controller;
-    });
+          Positioned(
+            top: 50.0,
+            right: 15.0,
+            left: 15.0,
+            child: Container(
+              height: 50.0,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(3.0),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(1.0, 5.0),
+                      blurRadius: 10,
+                      spreadRadius: 3)
+                ],
+              ),
+              child: TextField(
+                cursorColor: Colors.black,
+                controller: appState.locationController,
+                decoration: InputDecoration(
+                  icon: Container(
+                    margin: EdgeInsets.only(left: 20, top: 5),
+                    width: 10,
+                    height: 10,
+                    child: Icon(
+                      Icons.location_on,
+                      color: Colors.black,
+                    ),
+                  ),
+                  hintText: "pick up",
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+                ),
+              ),
+            ),
+          ),
+
+          Positioned(
+            top: 105.0,
+            right: 15.0,
+            left: 15.0,
+            child: Container(
+              height: 50.0,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(3.0),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(1.0, 5.0),
+                      blurRadius: 10,
+                      spreadRadius: 3)
+                ],
+              ),
+              child: TextField(
+                cursorColor: Colors.black,
+                controller: appState.destinationController,
+                textInputAction: TextInputAction.go,
+                onSubmitted: (value) {
+                  appState.sendRequest(value);
+                },
+                decoration: InputDecoration(
+                  icon: Container(
+                    margin: EdgeInsets.only(left: 20, top: 5),
+                    width: 10,
+                    height: 10,
+                    child: Icon(
+                      Icons.local_taxi,
+                      color: Colors.black,
+                    ),
+                  ),
+                  hintText: "destination?",
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+                ),
+              ),
+            ),
+          ),
+
+//        Positioned(
+//          top: 40,
+//          right: 10,
+//          child: FloatingActionButton(onPressed: _onAddMarkerPressed,
+//          tooltip: "aadd marker",
+//          backgroundColor: black,
+//          child: Icon(Icons.add_location, color: white,),
+//          ),
+//        )
+        ],
+      ),
+    );
   }
-
-
-
-  void _onCameraMove(CameraPosition position) {
-  }
-
-  void sendRequest(String value) {}
 }
-
 
 
 
